08e8edbd79e08ac4a8cf64c94ef4fbe64b8ed649
newoldname <thjbjs@outlook.com>
2024-03-26 22:38:34 +0900

    feat: 첨부파일 다운로드까지 됨. 이제 공고별 파일 다운도르 경로 설계 하면 됨

--- bus_notice.py
+++ bus_notice.py
@@ -7,6 +7,7 @@
 # pip3 install google-cloud-vision
 # pip3 install python-dotenv
 # pip3 install pytz
+# pip3 install PyMuPDF
 
 # DB - database name: smus
 # DB - collection name: bus_notice
@@ -23,6 +24,8 @@ import json
 from datetime import datetime
 import pytz
 
+import fitz
+import base64
 from google.cloud import vision
 import os
 from dotenv import load_dotenv
@@ -37,18 +40,6 @@ BUS_NUMBER_SET = set(BUS_NUMBER_LIST)
 
 def convertTime(timeString):
     return datetime.strptime(timeString, "%Y-%m-%d %H:%M:%S")
-
-# set형을 json 배열의 문자열 형태로 반환
-def setObject2JsonArrayString(setObject):
-    newStringList = []
-    for i in setObject:
-        newStringList.append('"' + i + '"')
-    return "[" + ", ".join(newStringList) + "]"
-
-# json 배열의 문자열을 set형태로 반환
-def jsonArrayString2SetObject(jsonString):
-    listObjest = json.loads(jsonString)
-    return set(listObjest)
 
 # 슬랙으로 메시지 보내기
 def sendMessageToSlack(message):
@@ -75,9 +66,9 @@ def getNormalErrorMessage(error):
     return json.dumps(errorMessage, ensure_ascii = False)
 
 # 이미지 다운로드 재시도 경고 메시지(준비중)
-def getImageDownloadRetryMessage(imgURL, retryTimes, error):
+def getFileDownloadRetryMessage(fileUrl, retryTimes, error):
     issueData = {
-        "imageURL": imgURL,
+        "imageURL": fileUrl,
         "retryTimes": retryTimes,
         "error": str(error)
     }
@@ -85,16 +76,16 @@ def getImageDownloadRetryMessage(imgURL, retryTimes, error):
         "Level": "warning",
         "ErrorHost":"BUS Crawling GCP",
         "Time": str(datetime.now(pytz.timezone('Asia/Seoul'))), #2023-10-20 13:00:32.447078+09:00
-        "WarningMessage": "사진을 다운로드할 때 연결을 거부당해서 재시도 중입니다. 크롤러가 너무 빈번하게 작동했거나 기존 연결을 끊지 않을 가능성이 있습니다. ",
+        "WarningMessage": "파일을 다운로드할 때 연결을 거부당해서 재시도 중입니다. 크롤러가 너무 빈번하게 작동했거나 기존 연결을 끊지 않을 가능성이 있습니다. ",
         "data": issueData
     }
     
     return json.dumps(warnMessage, ensure_ascii = False)
 
 # 이미지 다운로드 실패 에러 메시지
-def getImageDownloadErrorMessage(imgURL, error):
+def getFileDownloadErrorMessage(NoticeId, error):
     issueData = {
-        "imageURL": imgURL,
+        "imageURL": NoticeId,
         "error": str(error)
     }
     errorMessage = {
@@ -104,7 +95,7 @@ def getImageDownloadErrorMessage(imgURL, error):
         "WarningMessage": "사진을 다운로드할 때 연결을 거부당해서 살패했습니다. 크롤러가 너무 빈번하게 작동했거나 기존 연결을 끊지 않을 가능성이 있습니다. ",
         "data": issueData
     }
-    return json.dumps(errorMessage, ensure_ascii = False)
+    return json.dumps(errorMessage, ensure_ascii = False, indent=2)
 
 # 이미지 OCR 실패 에러 메시지
 def getOCRErrorMessage(imagePath, error):
@@ -121,40 +112,83 @@ def getOCRErrorMessage(imagePath, error):
     }
     return json.dumps(errorMessage, ensure_ascii = False)
 
-# ocr 사진 다운로드하기
-def downloadImage(url):
-    print("Start OCR: ", url)
-    imageName = url.split("/")[-1]
-    imagePath = f"./{imageName}"
+def getNoticeIds():
+    baseURL = f"https://topis.seoul.go.kr/notice/selectNoticeList.do"
+    baseBody = {
+        "pageIndex": 1,
+        "recordPerPage": 10,
+        "category": "sTtl",
+        "boardSearch": "우회"
+    }
+
+    responseJsonString = requests.post(baseURL, data=baseBody, verify=False).content
+    noticeDict = json.loads(responseJsonString)
+
+    noticeIds = []
+    for info in noticeDict["rows"]:
+        noticeIds.append(info["bdwrSeq"])
+    return noticeIds
+
+# 첨부파일 다운로드하기
+def downloadFiles(noticeId):
+    noticeId = str(noticeId)
+    print("Download Notice File: ", noticeId)
     
+    baseURL = f"https://topis.seoul.go.kr/notice/selectNoticeFileDown.do"
+    baseBody = {
+        "bdwrSeq": noticeId
+    }
+    
+    filePaths = []
     try:
-        img_data = requests.get(url, verify=False).content
+        responseJsonString = requests.post(baseURL, data=baseBody, verify=False).content
+        fileDict = json.loads(responseJsonString)
+        for fileDate in fileDict["rows"]:
+            fileBytes = bytes(fileDate["apndFile"], 'utf-8')
+            fileName = fileDate['apndFileNm']
+            filePath = f"./{fileName}"
+            with open(filePath, 'wb') as handler:
+                handler.write(base64.decodebytes(fileBytes))
+            filePaths.append(filePath)
     except Exception as e:
-        message = getImageDownloadErrorMessage(url, e)
+        message = getFileDownloadErrorMessage(noticeId, e)
         sendMessageToSlack(message)
         raise Exception(message)
-    
-    with open(imagePath, 'wb') as handler:
-        handler.write(img_data)
-        return imagePath
+    return filePaths
+
+downloadFiles('3998')
+
+# pdf 파일을 각각의 사진으로 자르기
+def pdf2images(pdfPath):
+    pdfPath = os.path.abspath(pdfPath)
+    doc = fitz.open(pdfPath)
+    folderDir = os.path.dirname(pdfPath)
+    pdfName = os.path.basename(pdfPath).rsplit('.', 1)[0]
+    imageFolderPath = os.path.join(folderDir, pdfName)
+    os.mkdir(imageFolderPath)
+    for i, page in enumerate(doc):
+        newImagePath = os.path.join(imageFolderPath, f"{i}.png")
+        img = page.get_pixmap()
+        img.save(newImagePath)
+    return imageFolderPath
 
 # ocr 사진 삭제하기  
-def deleteImage(imagePath):
-    os.remove(imagePath)
+def deleteFile(filePath):
+    os.remove(filePath)
 
 # ocr 요청하고 set으로 반환해주기
-def detect_text(path):
+def detect_text(imagePath):
     """Detects text in the file."""
     client = vision.ImageAnnotatorClient()
 
-    with open(path, "rb") as image_file:
+    with open(imagePath, "rb") as image_file:
         content = image_file.read()
 
     image = vision.Image(content=content)
 
     response = client.document_text_detection(image=image)
     if response.error.message:
-        message = getOCRErrorMessage(path, response.error.message)
+        message = getOCRErrorMessage(imagePath, response.error.message)
         raise Exception(message)
 
     texts = response.text_annotations[0].description
@@ -164,29 +198,41 @@ def detect_text(path):
 
 # 실제 ocr 전체 과정
 def googleOcrUrl(url):
-    imagePath = downloadImage(url)
-    resultSet = detect_text(imagePath)
-    deleteImage(imagePath)
+    filePath = downloadFile(url)
+    # TODO: 파일을 이미지 배열으로 바꿔서 반환하기
+    if filePath.rstrip('.', 1) != 'pdf':
+        return
+    imagesFolderPath = pdf2images(filePath)
+    
+    # TODO: 배열으로 되어 있는 한 파일의 여러 이미지를 각각 OCR해서 결과 반환하기
+    resultSet = set()
+    for root, dirs, files in os.walk(imagesFolderPath):
+        for fileName in files:
+            filePath = os.path.join(root, fileName)
+            resultSet.union(detect_text(filePath))
+            os.remove(filePath)
+    deleteFile(filePath)
+    os.rmdir(imagesFolderPath)
     return resultSet
 
 # db에 새로운 url 및 대응하는 set형으로 저장. TTL 추가 예정
 def saveSetResult(dbTable, url, resultSet): 
     dbTable.insert_one({
         "url": url,
-        "bus_number": setObject2JsonArrayString(resultSet)
+        "bus_number": list(resultSet)
     })
     
 # 해당 url이 db에 존재하면 set형, 없으면 None반환
-def getResultFromDB(dbTable, url):
-    dbResult = dbTable.find_one({"url": url})
-    if dbResult != None:
-        print("find History: ", url)
-        return jsonArrayString2SetObject(dbResult['bus_number'])
+def getResultFromHistoryDB(historyTable, url):
+    history = historyTable.find_one({"url": url})
+    if history != None:
+        print("Found History: ", url)
+        return set(history['bus_number'])
     else: 
         return None
 
 # URL으로 OCR를 요청하고, 그 결과를 Set형 버스 번호 정보로 반환 및 DB 저장하기
-def getResultFromUrl(urlTable, url):
+def getResultFromFileUrl(urlTable, url):
     googleOcrResult = googleOcrUrl(url)
     matchedBusNumberSet = set(googleOcrResult) & BUS_NUMBER_SET
     saveSetResult(urlTable, url, matchedBusNumberSet)
@@ -194,9 +240,9 @@ def getResultFromUrl(urlTable, url):
 
 # url를 입력하고 set형을 반환하기
 def getNumberSet(urlTable, url):
-    busResultSet = getResultFromDB(urlTable, url)
+    busResultSet = getResultFromHistoryDB(urlTable, url)
     if busResultSet == None:
-        busResultSet = getResultFromUrl(urlTable, url)
+        busResultSet = getResultFromFileUrl(urlTable, url)
     return busResultSet
 
 def getBusNotice(): 
@@ -275,9 +321,9 @@ def getBusNotice():
         
     print("Crawling Done. Time:", str(datetime.now(pytz.timezone('Asia/Seoul'))))
 
-if __name__ == "__main__":
-    getBusNotice()
-    schedule.every().hour.do(getBusNotice)
-    while True:
-            schedule.run_pending()
-            time.sleep(1)
+# if __name__ == "__main__":
+#     getBusNotice()
+#     schedule.every().hour.do(getBusNotice)
+#     while True:
+#             schedule.run_pending()
+#             time.sleep(1)
